07a7d4b63ad0c98279bc03ed4ddb256bb7aab42b
deflandr <deflandr@pc-dg-035-02.iut2.upmf-grenoble.fr>
2018-05-29 16:41:37 +0200

    Initial commit

--- src/Aventuriers/Messager.java
+++ src/Aventuriers/Messager.java
@@ -4,17 +4,18 @@ import ElementsJeu.Tuile;
 import Aventuriers.Aventurier;
 import Cartes.CarteAventurier;
 import Enumerations.EtatAventurier;
+import Enumerations.EtatTuile;
 import java.util.ArrayList;
 
 public class Messager extends Aventurier {
 
     // constructeurs
-    Messager(String nomJoueur, CarteAventurier carteaventurier){
+    Messager(String nomJoueur, CarteAventurier carteaventurier) {
         setNomJoueur(nomJoueur);
         setEtat(EtatAventurier.vivant);
         setCarteAventurier(carteaventurier);
     }
-    
+
     // autres
     @Override
     public ArrayList<Tuile> calculDeplacementPos() {
@@ -22,11 +23,32 @@ public class Messager extends Aventurier {
         int posXactuelle = tuileActuelle.getPosX();
         int posYactuelle = tuileActuelle.getPosY();
         ArrayList<Tuile> tuilesPossible = new ArrayList<>();
-        
-        if (posXactuelle != 1){
-            Tuile tuilleGauche =
+
+        if (posXactuelle != 1) {
+            Tuile tuileGauche = tuileActuelle.getGrille().getTuile(posXactuelle - 1, posYactuelle);
+            if (tuileGauche.getEtat() != EtatTuile.submergee) {
+                tuilesPossible.add(tuileGauche);
+            }
         }
-        
+        if (posXactuelle != 6) {
+            Tuile tuileDroite = tuileActuelle.getGrille().getTuile(posXactuelle + 1, posYactuelle);
+            if (tuileDroite.getEtat() != EtatTuile.submergee) {
+                tuilesPossible.add(tuileDroite);
+            }
+        }
+        if (posYactuelle != 1) {
+            Tuile tuileHaut = tuileActuelle.getGrille().getTuile(posXactuelle, posYactuelle - 1);
+            if (tuileHaut.getEtat() != EtatTuile.submergee) {
+                tuilesPossible.add(tuileHaut);
+            }
+        }
+        if (posYactuelle != 6) {
+            Tuile tuileBas = tuileActuelle.getGrille().getTuile(posXactuelle, posYactuelle + 1);
+            if (tuileBas.getEtat() != EtatTuile.submergee) {
+                tuilesPossible.add(tuileBas);
+            }
+        }
+
         return tuilesPossible;
     }
 
