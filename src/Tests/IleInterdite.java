1372fe3beee70107ad47e6906664bb2bef6f0b5e
deflandr <deflandr@pc-dg-035-02.iut2.upmf-grenoble.fr>
2018-05-29 12:10:03 +0200

    Initial commit

--- src/Tests/IleInterdite.java
+++ src/Tests/IleInterdite.java
@@ -15,17 +15,7 @@ public class IleInterdite {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    
-        System.out.println("Marie");
-        System.out.println("Marie essai 2");
         
-        System.out.println("Roxane");
-        System.out.println("Roxane2");
-        System.out.println("Corentin Paradis");
-      
-        System.out.println("Pierre Fournier");
-
     }
     
 }
