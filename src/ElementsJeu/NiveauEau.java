ddc78ad6244a91dc415563fc7f297313d0f781c9
deflandr <deflandr@pc-dg-035-02.iut2.upmf-grenoble.fr>
2018-05-29 12:50:55 +0200

    Initial commit

--- src/ElementsJeu/NiveauEau.java
+++ src/ElementsJeu/NiveauEau.java
@@ -2,7 +2,33 @@ package ElementsJeu;
 
 public class NiveauEau {
 
-	private int maximum;
-	private int niveau;
+    // attributs
+    private int maximum;
+    private int niveau;
 
+    // constructeurs
+    NiveauEau(int maximum, int niveau) {
+        setMaximum(maximum);
+        setNiveau(niveau);
+    }
+
+    // setteurs
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    // getteurs
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    // autres
 }
