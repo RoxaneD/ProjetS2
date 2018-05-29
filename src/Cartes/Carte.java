772d0089b3b419bef454bffb22c624e9ebc00446
deflandr <deflandr@pc-dg-035-02.iut2.upmf-grenoble.fr>
2018-05-29 15:18:42 +0200

    Initial commit

--- src/Cartes/Carte.java
+++ src/Cartes/Carte.java
@@ -1,11 +1,30 @@
 package Cartes;
 
-
 import Enumerations.EtatCarte;
 
 public abstract class Carte {
 
-	private EtatCarte etat;
-	private String description;
+    // attributs
+    private EtatCarte etat;
+    private String description;
+
+    // setteurs
+    public void setEtat(EtatCarte etat) {
+        this.etat = etat;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    // getteurs
+
+    public EtatCarte getEtat() {
+        return etat;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    // autres
 
 }
