c8e2e8d402fb4e2a6f8cd263d47535216071609c
fourniep <fourniep@pc-dg-035-11.iut2.upmf-grenoble.fr>
2018-05-29 17:01:23 +0200

    Merge origin/master
    Conflicts:
    	src/Tests/IleInterdite.java

--- src/Aventuriers/Explorateur.java
+++ src/Aventuriers/Explorateur.java
@@ -4,6 +4,7 @@ import ElementsJeu.Tuile;
 import Aventuriers.Aventurier;
 import Cartes.CarteAventurier;
 import Enumerations.EtatAventurier;
+import Enumerations.EtatTuile;
 import java.util.ArrayList;
 
 public class Explorateur extends Aventurier {
@@ -20,17 +21,47 @@ public class Explorateur extends Aventurier {
     
     @Override
     public ArrayList<Tuile> calculDeplacementPos() {
+             ArrayList<Tuile> tuilePossible = new ArrayList<>();
+
+        
         int posX = this.getTuile().getPosX();
         int posY = this.getTuile().getPosY();
-        if(posX ==1){
-      
+        if(posX != 1){
+           Tuile T = getTuile().getGrille().getTuile(posX -1, posY);
+           if ( T.getEtat() != EtatTuile.submergee ){
+              tuilePossible.add(T);
+           }
         }
+        
+        if(posX != 6){
+            Tuile T = getTuile().getGrille().getTuile(posX +1, posY);
+           if ( T.getEtat() != EtatTuile.submergee ){
+              tuilePossible.add(T);
+           }
+        }
+        
+        if(posY != 1){
+            Tuile T = getTuile().getGrille().getTuile(posX, posY -1);
+           if ( T.getEtat() != EtatTuile.submergee ){
+              tuilePossible.add(T);
+           }
+        }
+        
+        if(posY != 6){
+            Tuile T = getTuile().getGrille().getTuile(posX, posY +1 );
+           if ( T.getEtat() != EtatTuile.submergee ){
+              tuilePossible.add(T);
+           }
+        }
+        
+        return tuilePossible;
         
         
     }
 
     @Override
     public ArrayList<Tuile> calculAssechementPos() {
+        return null;
         
     }
 
