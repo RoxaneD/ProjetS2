64a4420168de9b0aea6265458c1e30e224ed9492
deflandr <deflandr@pc-dg-035-02.iut2.upmf-grenoble.fr>
2018-05-29 11:36:06 +0200

    Initial commit

--- src/Tests/IleInterdite.java
+++ src/Tests/IleInterdite.java
@@ -20,7 +20,7 @@ public class IleInterdite {
         System.out.println("Marie");
         
         System.out.println("Roxane");
-        System.out.println("Roxane-2");
+        System.out.println("Roxane-22");
         System.out.println("Corentin");
       
         System.out.println("Pierre Fournier");
