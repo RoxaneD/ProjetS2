f8a58fde6e7956f0f3e5ae489394c8151eb423cf
dechambm <dechambm@pc-dg-035-01.iut2.upmf-grenoble.fr>
2018-05-30 12:13:05 +0200

    Merge origin/master
    Conflicts:
    	src/Tests/IleInterdite.java

--- src/Util/Parameters.java
+++ src/Util/Parameters.java
@@ -1,11 +1,5 @@
-package Util;
 
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package util;
+package Util;
 
 import java.awt.Color;
 
