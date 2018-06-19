1a6672517920392a92a6716e0f57b91cf51ba09a
deflandr <deflandr@pc-dg-035-02.iut2.upmf-grenoble.fr>
2018-06-19 13:10:00 +0200

    Initial commit

--- src/Enumerations/NomTuile.java
+++ src/Enumerations/NomTuile.java
@@ -1,28 +1,28 @@
 package Enumerations;
 //Tout les noms des differentes tuiles
 public enum NomTuile {
-	LePontDesAbimes,
-	LaPorteDeBronze,
-	LaCaverneDesOmbres,
-	LaPorteDeFer,
-	LaPortedOr,
-	LesFalaisesDeLOubli,
-	LePalaisDeCorail,
-	LaPortedArgent,
-	LesDunesDeLillusion,
-	Heliport,
-	LaPorteDeCuivre,
-	LeJardinDesHurlements,
-	LaForetPourpre,
-	LeLagonPerdu,
-	LeMaraisBrumeux,
-	Observatoire,
-	LeRocherFantome,
-	LaCaverneDuBraisier,
-	LeTempleDuSoleil,
-	LeTempleDeLaLune,
-	LePalaisDesMarees,
-	LeValDuCrepuscule,
-	LaTourDuGuet,
+	LePontDesAbimes,//
+	LaPorteDeBronze,//
+	LaCaverneDesOmbres,//
+	LaPorteDeFer,//
+	LaPortedOr,//
+	LesFalaisesDeLOubli,//
+	LePalaisDeCorail,//
+	LaPortedArgent,//
+	LesDunesDeLillusion,//
+	Heliport,//
+	LaPorteDeCuivre,//
+	LeJardinDesHurlements,//
+	LaForetPourpre,//
+	LeLagonPerdu,//
+	LeMaraisBrumeux,//
+	Observatoire,//
+	LeRocherFantome,//
+	LaCaverneDuBraisier,//
+	LeTempleDuSoleil,//
+	LeTempleDeLaLune,//
+	LePalaisDesMarees,//
+	LeValDuCrepuscule,//
+	LaTourDuGuet,//
 	LeJardinDesMurmures
 }
